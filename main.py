f5c77fe96a94439565f56b6bc96283367a316273
admin-farhan <muhfarhan912391@gmail.com>
2023-09-29 12:30:13 +0700

    finish main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 A = tf.constant([[4, 3], [6, 1]])
 B = tf.constant([[1, 2], [3, 4]])
+C = tf.constant(4, shape=[1, 2], dtype=tf.int16)
+# D = tf.int16([[1, 2], [3, 4]])
 
 tensor_zero = tf.zeros(shape=[3, 4], dtype=tf.float32)
 tensor_one = tf.ones(shape=[3, 4], dtype=tf.float32)
@@ -17,6 +20,13 @@ tensor_transpose = tf.transpose(AB_concat)
 tensor_matmul = tf.matmul(AB_concat, tensor_transpose)
 
 tensor_identity = tf.eye(3, 3, dtype=tf.int32)
+tensor_add = tf.add(A, B)
+tensor_sub = tf.subtract(A, B)
+tensor_div = tf.divide(A, B)
 
-print(tensor_identity)
+# INDEXING
+# tf.constant
+# tf.gather
+
+print(tensor_div)
 # print(AB_concat)
